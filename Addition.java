56fca499f1fcd76e6c23a4056900a16847b98cfb
rgarrity62 <rebecca.garrity@ufl.edu>
2022-03-14 23:09:34 -0500

    include subtraction in Addition.java

--- Addition.java
+++ Addition.java
@@ -6,15 +6,30 @@ public class Addition {
         Scanner scnr = new Scanner(System.in);
         int numberOne;
         int numberTwo;
+        int numberThree;
+        int numberFour;
         int sum;
+        int diff;
 
-        System.out.print("Enter first number: ");
+        //Addition
+        System.out.print("Enter a number: ");
         numberOne = scnr.nextInt();
-        System.out.print("Enter second number: ");
+        System.out.print("Enter another number: ");
         numberTwo = scnr.nextInt();
 
         sum = numberOne + numberTwo;
         System.out.println("The sum is: " + sum);
+
+        //sSubtraction
+        System.out.print("Enter a number: ");
+        numberThree = scnr.nextInt();
+        System.out.print("Enter another number: ");
+        numberFour = scnr.nextInt();
+
+        diff = numberThree - numberFour;
+        System.out.println("The difference is: " + diff);
+
+
 
 
     }
